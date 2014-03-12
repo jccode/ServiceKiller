49de966b4191481afd5a2f4cf78fd3d30ad77287
jcchen <junchangchen@gmail.com>
2014-03-12 23:32:08 +0800

    complete add apps view

--- src/tk/jcchen/servicekiller/ui/AppGridActivity.java
+++ src/tk/jcchen/servicekiller/ui/AppGridActivity.java
@@ -7,30 +7,36 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.ActionMode;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Checkable;
+import android.widget.FrameLayout;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AppGridActivity extends Activity {
 	
 	GridView appGrid;
 	private List<ResolveInfo> mApps;
-	private final int IMG_WIDTH = 100;
+	private final int IMG_WIDTH = 80;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +48,7 @@ public class AppGridActivity extends Activity {
 		setupActionBar();
 		
 		appGrid = (GridView) findViewById(R.id.grid_apps);
-		appGrid.setAdapter(new AppsAdapter());
+		appGrid.setAdapter(new AppsAdapter(this));
 		appGrid.setChoiceMode(GridView.CHOICE_MODE_MULTIPLE_MODAL);
 		appGrid.setMultiChoiceModeListener(new MultiChoiceModeListener());
 	}
@@ -60,7 +66,7 @@ public class AppGridActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.app_grid, menu);
+//		getMenuInflater().inflate(R.menu.app_grid, menu);
 		return true;
 	}
 
@@ -90,7 +96,10 @@ public class AppGridActivity extends Activity {
 	
 	public class AppsAdapter extends BaseAdapter {
 		
-		public AppsAdapter() {
+		private LayoutInflater mLayoutInflater;
+		
+		public AppsAdapter(Context context) {
+			mLayoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		}
 
 		@Override
@@ -111,29 +120,42 @@ public class AppGridActivity extends Activity {
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			
-			CheckableImageView i;
+			ActivityInfo info = mApps.get(position).activityInfo;
+			Drawable img = info.loadIcon(getPackageManager());
+			CharSequence name = info.loadLabel(getPackageManager());
+			
+			CheckableView view;
 			if(convertView == null) {
-				i = new CheckableImageView(AppGridActivity.this);
-				i.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                	i.setLayoutParams(new GridView.LayoutParams(IMG_WIDTH, IMG_WIDTH));
+				view = new CheckableView(AppGridActivity.this, parent);
 			} else {
-				i = (CheckableImageView)convertView;
+				view = (CheckableView) convertView;
 			}
-			
-			ResolveInfo info = mApps.get(position);
-			i.setImageDrawable(info.activityInfo.loadIcon(getPackageManager()));
-			
-			return i;
+			view.setView(img, name);
+			return view;
 		}
 		
 	}
 	
-	public class CheckableImageView extends ImageView implements Checkable {
-		
+	
+	public class CheckableView extends FrameLayout implements Checkable {
+
 		private boolean mChecked;
+		private FrameLayout mView;
 		
-		public CheckableImageView(Context context) {
+		public CheckableView(Context context, ViewGroup parent) {
 			super(context);
+			mView = (FrameLayout) getLayoutInflater().inflate(R.layout.app_grid_cell, parent, false);
+			this.addView(mView);
+		}
+		
+		public CheckableView(Context context, ViewGroup parent, Drawable img, CharSequence name) {
+			this(context, parent);
+			setView(img, name);
+		}
+		
+		protected void setView(Drawable img, CharSequence name) {
+			((ImageView)mView.findViewById(R.id.app_img)).setImageDrawable(img);
+			((TextView)mView.findViewById(R.id.app_name)).setText(name);
 		}
 
 		@Override
@@ -145,36 +167,19 @@ public class AppGridActivity extends Activity {
 		public void setChecked(boolean checked) {
 			mChecked = checked;
 			
-			// set style of checked. repaint
-			invalidate();
+			if(checked) {
+				mView.findViewById(R.id.app_overlay).setVisibility(VISIBLE);
+			} else {
+				mView.findViewById(R.id.app_overlay).setVisibility(INVISIBLE);
+			}
 		}
 
 		@Override
 		public void toggle() {
 			setChecked(!mChecked);
 		}
-
-		@Override
-		protected void onDraw(Canvas canvas) {
-			super.onDraw(canvas);
-			if(mChecked)
-				drawOverlay(canvas);
-		}
-		
-		private void drawOverlay(Canvas canvas) {
-			Paint paint = new Paint();
-			paint.setColor(Color.CYAN);
-			paint.setStyle(Paint.Style.STROKE);
-			paint.setStrokeWidth(10);
-			canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
-			
-			paint.setStyle(Paint.Style.FILL);
-			paint.setAlpha(80);
-			canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
-		}
 		
 	}
-	
 	
 	public class MultiChoiceModeListener implements GridView.MultiChoiceModeListener {
 		
